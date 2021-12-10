302222ccafe623fbf6450f6030f3ade4e0280251
lmharb <lm.harb59@gmail.com>
2021-12-10 11:45:19 +0100

    Added Contacts filtering
    - New Filtered contacts state
    - Filter & clearFilter in context
    - Updated contacts' display based on filtered

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -41,6 +41,7 @@ const ContactState = (props) => {
       },
     ],
     current: null,
+    filtered: null,
   }
 
   const [state, dispatch] = useReducer(contactReducer, initialState)
@@ -71,19 +72,27 @@ const ContactState = (props) => {
   }
 
   // Filter Contacts
-
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })
+  }
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })
+  }
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
+        filterContacts,
+        clearFilter,
       }}
     >
       {props.children}
