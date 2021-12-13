f491ab9d8535d9a20f07a76e1401583677ae9dfe
lmharb <lm.harb59@gmail.com>
2021-12-13 14:07:47 +0100

    Set Alert Notifications:
    - Implemented Alert Context
    - Created Alert component to display alerts

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,34 @@ import Home from "./components/pages/Home"
 import About from "./components/pages/About"
 import Register from "./components/auth/Register"
 import Login from "./components/auth/Login"
+import Alerts from "./components/layout/Alerts"
 
 import ContactState from "./context/contact/ContactState"
 import AuthState from "./context/auth/AuthState"
+import AlertState from "./context/alert/AlertState"
 import "./App.css"
 
 const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                {/* <Route path='*' element={<NotFound />} /> */}
-              </Routes>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/register' element={<Register />} />
+                  <Route path='/login' element={<Login />} />
+                  {/* <Route path='*' element={<NotFound />} /> */}
+                </Routes>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   )
